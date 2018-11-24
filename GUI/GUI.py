495c5beb0a7fa2f9cf2788ba47f15ef653d054e9
wkaiser <wkaisertexas@gmail.com>
2018-11-24 15:30:19 -0600

    ...

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,1 +1,28 @@
 # this is the absolute GUI file for the HEX program
+import turtle
+
+
+
+
+
+
+
+
+class Space:  # this should be a part of the game logic file, not the GUI file
+    white = False
+    black = False
+
+    def __init__(self):
+        return
+
+    def white_space(self):
+        self.white = True
+        self.black = False
+
+    def black_space(self):
+        self.white = False
+        self.black = True
+
+    def blank_space(self):
+        self.black = False
+        self.white = False
