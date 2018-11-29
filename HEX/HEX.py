8ecbeb3897da97c96716c89a83a7e03083882530
William Kaiser <wkaisertexas@gmail.com>
2018-11-29 12:39:45 -0600

    Update HEX.py

--- HEX/HEX.py
+++ HEX/HEX.py
@@ -4,16 +4,20 @@
 class Board:
     tile_array = None
     groups = []  # these are groups of pieces that are connected
-
+    moves = []  # this is
+    
     def __init__(self, size):
         self.tile_array = [[Space()] * size] * size
 
     def white_move(self, x, y):
-        return
+        ((Space)self.tile_array[x][y]).white_space()
 
     def black_move(self, x, y):
-        return
+        ((Space)self.tile_array[x][y]).black_space()
 
+    def expand_groups(self):
+        return
+        
     def calculate_win_condition(self):
         # this will not return which team one since that is the player that currently moved
         return
@@ -36,3 +40,17 @@ class Space:  # this should be a part of the game logic file, not the GUI file
     def blank_space(self):
         self.black = False
         self.white = False
+      
+class move:  # this is used for storage of the moves in the move array
+    x = None
+    y = None
+    white = False
+    black = False
+    def __init__(self, x, y, white):
+        self.x = x
+        self.y = y
+        if white:
+            self.white = True
+        else:
+            self.black = True
+    
