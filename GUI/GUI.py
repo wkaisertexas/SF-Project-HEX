869f1420361454493cb8f0f54e3592846fc01862
William Kaiser <wkaisertexas@gmail.com>
2018-12-07 11:47:58 -0600

    Update GUI.py

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,10 +1,21 @@
 # this is the absolute GUI file for the HEX program
 import turtle
+import tkinker as tk  # this is for the GUI package
 
-def draw_grid(size):
-    for x in range(size):
-        for y in range(size):
-            return
+# constants
+screen_x = 1000
+screen_y = 2000
+
+class Window:  # 
+    win = None 
+    
+    def __init__(self):
+        self.win = tk.Tk()
+
+    def draw_grid(size):
+        for x in range(size):
+            for y in range(size):
+                return
 
 
 
